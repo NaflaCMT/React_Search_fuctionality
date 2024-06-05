53dc319d2d6f016f08d356e97637c71bf15959e2
Nafla CMT <naflacmt@gmail.com>
2024-06-05 12:35:04 +0530

    refactor:style

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ export class App extends Component {
 
     return (
       <div>
-        <h1 className="app-title">Monsters</h1>
+        <h1 className="app-title">FRIENDS</h1>
         <input
           type="search"
           placeholder="search"
@@ -52,23 +52,25 @@ export class App extends Component {
           className="number"
           onChange={this.onImageChange}
         />
-
-        <div className="card-list">
-          {filteredUser.map((user) => (
-            <div className="card-container" key={user}>
-              {console.log(user.id)}
-              <div>
-                <img
-                  src={`https://robohash.org/${user.id}?set=set${this.state.imagefield}`}
-                  className="img"
-                />
+        {filteredUser.length > 0 ? (
+          <div className="card-list">
+            {filteredUser.map((user) => (
+              <div className="card-container" key={user}>
+                <div>
+                  <img
+                    src={`https://robohash.org/${user.id}?set=set${this.state.imagefield}`}
+                    className="img"
+                  />
+                </div>
+                <div className="names">
+                  <h3>{user.name}</h3>
+                </div>
               </div>
-              <div className="names">
-                <h3>{user.name}</h3>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <h1>Search Not Found</h1>
+        )}
       </div>
     );
   }
